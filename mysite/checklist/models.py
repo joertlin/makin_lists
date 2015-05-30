37ca3dba31bb192d01d67d8136930c84976b2651
Jeremiah Oertling <Jeremiah.Oertling@gmail.com>
2015-05-29 20:27:06 -0500

    Added 2 more classes to the model.  will set up a githup repo immediaetly.

--- mysite/checklist/models.py
+++ mysite/checklist/models.py
@@ -4,15 +4,18 @@ from django.contrib.auth.models import User
 from django.utils import timezone
 import datetime
 
-class check_lists(models.Model):
+class Checklist(models.Model):
 	"""
 	This is the table that identifies lists that the user has generated.
 	"""
-	list_id = models.AutoField(primary_key=True)
+	list_id = models.AutoField(primary_key  =True)
 	list_name = models.CharField(max_length = 10)
 	date_created = models.DateTimeField('date list was created')
 	date_completed = models.DateTimeField('date list was completed',null = True)
 	user = models.ForeignKey(User)
+
+	def __str__(self):
+		return self.list_name
 
 	def is_a_complete_list(self):
 		if self.date_completed == 'null':
@@ -20,27 +23,30 @@ class check_lists(models.Model):
 		else:
 			return True 
 
+class ListItems(models.Model):
+	"""
+	This is the table with all of the lists that users generate.
+	"""
+	list_id = models.ForeignKey(Checklist)
+	item_id = models.ManyToManyField(AllItems)
+	item_quantity = models.CharField(max_length = 10)
+	date_checked_off = models.DateTimeField('date item was checked off', null = True)
+	
+	def __str__(self):
+		return self.item_quantity
 
+	def is_a_checked_off_item(self):
+		if self.date_checked_off == 'null':
+			return True
+		else:
+			return False
 
-#class Question(models.Model):
-#	question_text = models.CharField(max_length = 200)
-#	pub_date = models.DateTimeField('date published')
-#
-#	def __str__(self):
-#		return self.question_text
-#
-#	def was_published_recently(self):
-#		now = timezone.now()
-#		return now - datetime.timedelta(days = 1) <= self.pub_date <= now
-#
-#	was_published_recently.admin_order_field = 'pub_date'
-#	was_published_recently.boolean = True
-#	was_published_recently.short_description = 'Published recently?'
-#
-#class Choice(models.Model):
-#	question = models.ForeignKey(Question)
-#	choice_text = models.CharField(max_length = 200)
-#	votes = models.IntegerField(default = 0)
-#
-#	def __str__(self):
-#		return self.choice_text
+class AllItems(models.Model):
+	"""
+	This is a list of all the items users have ever input into any list.
+	"""
+	item_id = models.AutoField(primary_key = True)
+	item_name = models.CharField(max_length = 10)
+
+	def __str__(self):
+		return self.item_name
